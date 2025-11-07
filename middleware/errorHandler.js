f369d03bce56141abe2d92888512a7d71eeab135
itsaot <calvinmzoboshe@gmail.com>
2025-11-07 12:19:25 +0200

    Error Handler

---
+++ middleware/errorHandler.js
@@ -0,0 +1,14 @@
+// Error handler middleware
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack); // log error stack for debugging
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
+
+module.exports = { errorHandler };
